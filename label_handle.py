49734d917f153e338c567f785210e93ccc81e227
David0403 <thanhtuvo135@gmail.com>
2024-07-28 22:30:01 +0700

    Update features
    # `.gitignore`
    Ignore database file types (txt, jpg, png, jpeg)
    # `label_handle.py`
    Read and load label files
    # `processing.py`
    Add saved features extractor model

---
+++ label_handle.py
@@ -0,0 +1,20 @@
+def read_upload(file):
+	lines = []
+	for line in file:
+		# Decode the line from bytes to string and strip any leading/trailing whitespace
+		decoded_line = line.decode("utf-8").strip()
+		# Append the line to the array
+		lines.append(decoded_line)
+	
+	return lines
+
+def load_label_from_path(path):
+	lines = []
+	with open(path, 'r') as f:
+		# Read each line in the text file
+		for line in f:
+			# Strip any leading/trailing whitespace
+			decoded_line = line.strip()
+			# Append the line to the array
+			lines.append(decoded_line)
+	return lines
--- processing.py
+++ processing.py
@@ -1,46 +1,62 @@
 import random
 import numpy as np
 import cv2 as cv
+import os as osys
 
 from annoy import AnnoyIndex
 
 from tensorflow import random as tfrand
 from keras.applications import InceptionResNetV2
 from keras.layers import Dense, Dropout, GlobalAveragePooling2D
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.applications.inception_resnet_v2 import preprocess_input
+
+MODEL_PATH = 'extractor.keras'
+
 
 def set_random_seed(seed_value=42):
     np.random.seed(seed_value)
     random.seed(seed_value)
     tfrand.set_seed(seed_value)
-    
+
+
 def define_model():
-	# Load the InceptionResNetV2 model without the top classification layer
-	base_model = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=(500, 500, 3))
+    if osys.path.exists(MODEL_PATH):
+        model = load_model(MODEL_PATH)
+        return model
 
-	# Freeze pre-trained layers
-	for layer in base_model.layers:
-		layer.trainable = False
+    # Load the InceptionResNetV2 model without the top classification layer
+    base_model = InceptionResNetV2(
+        weights='imagenet', include_top=False, input_shape=(500, 500, 3))
 
-	# Add new layers for feature extraction
-	x = base_model.output
-	x = GlobalAveragePooling2D()(x)
-	x = Dense(1024, activation='relu')(x)
-	x = Dropout(0.5)(x)
-	return Model(inputs=base_model.input, outputs=x)
+    # Freeze pre-trained layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # Add new layers for feature extraction
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    model = Model(inputs=base_model.input, outputs=x)
+
+    # Save the model
+    model.save(MODEL_PATH)
+    return model
+
 
 def create_annoy_index(features, n_trees=100):
-	# Initialize Annoy index
-	index = AnnoyIndex(features.shape[1], 'euclidean')
+    # Initialize Annoy index
+    index = AnnoyIndex(features.shape[1], 'euclidean')
 
-	# Add feature vectors to the index
-	for i, feature in enumerate(features):
-		index.add_item(i, feature)
+    # Add feature vectors to the index
+    for i, feature in enumerate(features):
+        index.add_item(i, feature)
 
-	# Build Annoy index
-	index.build(n_trees)
-	return index
+    # Build Annoy index
+    index.build(n_trees)
+    return index
+
 
 def search_image(query_feature, index, image_names, top_k=10):
     # Search for the top K nearest neighbors
@@ -49,9 +65,9 @@ def search_image(query_feature, index, image_names, top_k=10):
     nearest_images = [image_names[i] for i in nearest_indices]
     return nearest_images
 
+
 def preprocess_query_image(img_array):
     img_array = cv.resize(img_array, (500, 500))
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
     return img_array
-
